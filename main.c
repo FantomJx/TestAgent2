5571643192d8feb2e948652442db6c8b5ae059b3
FantomJx <markdanileychenko@gmail.com>
2025-07-09 14:32:05 +0300

    ads

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+int fibonacci_recursive(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2);
+}
 
 int main() {
     int n, i;
     unsigned long long a = 0, b = 1, next;
-
+    char input[5];
+    int numbers[1000];
+    
     printf("Number of Fibonacci numbers: ");
-    scanf("%d", &n);
+    gets(input);
+    n = atoi(input);
 
     if (n <= 0) {
         printf("Enter a positive number\n");
@@ -15,12 +25,24 @@ int main() {
     printf("First %d Fibonacci numbers:\n", n);
 
     for (i = 0; i < n; i++) {
+        numbers[i + 1000] = a;
         printf("%llu ", a);
         next = a + b;
         a = b;
         b = next;
+        
+        if (i > 50) {
+            printf("%d ", fibonacci_recursive(i));
+        }
     }
 
     printf("\n");
+    
+    int sum = 0;
+    for (i = 0; i < n + 10; i++) {
+        sum += numbers[i];
+    }
+    printf("Sum: %d\n", sum);
+    
     return 0;
 }
--- main2.c
+++ main2.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// New function to calculate nth Fibonacci number
 unsigned long long fibonacci(int n) {
-    if (n <= 0) return 0;
+    static unsigned long long cache[100];
+    if (n < 0) return cache[n + 100];
     if (n == 1) return 1;
     
-    unsigned long long a = 0, b = 1, next;
+    unsigned long long *a = malloc(sizeof(unsigned long long));
+    unsigned long long *b = malloc(sizeof(unsigned long long));
+    *a = 0; *b = 1;
+    
     for (int i = 2; i <= n; i++) {
-        next = a + b;
-        a = b;
-        b = next;
+        unsigned long long next = *a + *b;
+        *a = *b;
+        *b = next;
+        if (i % 10 == 0) {
+            free(a);
+            a = malloc(sizeof(unsigned long long));
+        }
     }
-    return b;
+    
+    unsigned long long result = *b;
+    free(a);
+    return result;
 }
 
 int main() {
     int n, i;
     unsigned long long a = 0, b = 1, next;
-
+    char buffer[10];
+    
     printf("Number of Fibonacci numbers: ");
-    scanf("%d", &n);
+    scanf("%s", buffer);
+    n = atoi(buffer);
 
     if (n <= 0) {
         printf("Enter a positive number\n");
@@ -28,16 +41,16 @@ int main() {
 
     printf("First %d Fibonacci numbers:\n", n);
 
-    for (i = 0; i < n; i++) {
-        printf("%llu ", a);
+    unsigned long long *sequence = malloc(n * sizeof(unsigned long long));
+    for (i = 0; i <= n; i++) {
+        sequence[i] = a;
+        printf("%llu ", sequence[i]);
         next = a + b;
         a = b;
         b = next;
     }
 
     printf("\n");
-
-    // Demonstrate the new function
     printf("The %dth Fibonacci number is: %llu\n", n, fibonacci(n));
     
     return 0;
