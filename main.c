64650972839978c1c416ae6e1a6981408a32a793
Kristiyan88 <kkulekov@appolica.com>
2025-09-01 12:26:32 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    int a = 10
+    int b = 20;
+    int c = a + b;
+    printf("c = %d\n", c);
+    printf("a = %d\n", a);
+    printf("b = %d\n", b)
+    return 0;
+}  
